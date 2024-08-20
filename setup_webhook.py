0d5ff8aff887d2f281697c49a39c79fe409d2813
FooNicholas <foosekwoon@gmail.com>
2024-08-21 01:59:47 +0800

    minor changes to vercel.json file and setup_webhook

--- setup_webhook.py
+++ setup_webhook.py
@@ -5,7 +5,7 @@ from dotenv import load_dotenv
 load_dotenv()
 
 BOT_TOKEN = os.getenv("BOT_TOKEN")
-WEBHOOK_URL = f"https://cardboard-scraper.vercel.app/api/telegram_bot"
+WEBHOOK_URL = f"https://cardboard-scraper.vercel.app/api/bot"
 
 def set_webhook():
     response = requests.get(f"https://api.telegram.org/bot{BOT_TOKEN}/setWebhook?url={WEBHOOK_URL}")
